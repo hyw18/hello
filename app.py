fa62dccc622df3682e21cb69a803e68e86235c78
hyw18 <whduddn2008@gmail.com>
2026-04-15 10:49:36 +0900

    init

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    skills = [
+        "C", "C++", "C#", "Java", "Kotlin"
+    ]
+    return render_template('index.html', data=skills)
+
+@app.route('/send', methods=['POST'])
+def send():
+    skill = request.form.get('skill')
+    level = request.form.get('level')
+    return skill + " " + level
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+print("te")
