82ea1ee4f5b9276f1ae7848080fbdbbe48fcafd8
hyw18 <whduddn2008@gmail.com>
2026-04-21 12:07:31 +0900

    4.21항쟁

--- app.py
+++ app.py
@@ -1,19 +1,23 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
+messages = []
 
 @app.route('/')
 def home():
-    skills = [
-        "C", "C++", "C#", "Java", "Kotlin"
+    skill = [
+        "C", "C++", "C#", "Java",
+        "JavaScript",
+        "Python"
     ]
-    return render_template('index.html', data=skills)
+    return render_template('index.html', data=skill, messages=messages)
 
 @app.route('/send', methods=['POST'])
 def send():
     skill = request.form.get('skill')
     level = request.form.get('level')
-    return skill + " " + level
+    messages.append(skill + " / " + level)
+    return redirect('/')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
