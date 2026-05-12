7dd6ae945c0bd8cb91541a93cedfdb12292b479a
hyw18 <whduddn2008@gmail.com>
2026-05-12 14:46:30 +0900

    5.12 사변

--- app.py
+++ app.py
@@ -1,27 +1,55 @@
 from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
-messages = messages = [
-    {"skill": "Python"},
-    {"skill": "HTML"},
-    {"skill": "SQL"},
-    {"skill": "Bash"},
-    {"skill": "Java"}
-]
+messages = []
 
 @app.route('/')
 def home():
-    return render_template('index.html', messages=messages)
+    skill = [
+        "C", "C++", "C#", "Java",
+        "JavaScript",
+        "Python"
+    ]
+    return render_template('index.html', data=skill, messages=messages)
 
 @app.route('/send', methods=['POST'])
 def send():
     skill = request.form.get('skill', '').strip()
+    level = request.form.get('level', '').strip()
+    status = request.form.get('status', '').strip()
 
-    if not skill:
+    if not skill or not level or not status:
         return redirect('/')
-    
-    messages.append({"skill": skill})
-    
+
+    messages.append({
+        "skill": skill,
+        "level": level,
+        "status": status
+    })
+
+    return redirect('/')
+
+@app.route('/delete', methods=['POST'])
+def delete():
+    index = int(request.form['index'])
+    messages.pop(index)
+    return redirect('/')
+
+@app.route('/delete_all', methods=['POST'])
+def delete_all():
+    messages.clear()
+    return redirect('/')
+
+@app.route('/delete_selected', methods=['POST'])
+def delete_selected():
+    indexes = request.form.getlist('delete_indexes')
+
+    indexes = [int(index) for index in indexes]
+    indexes.sort(reverse=True)
+
+    for index in indexes:
+        messages.pop(index)
+
     return redirect('/')
 
 if __name__ == "__main__":
